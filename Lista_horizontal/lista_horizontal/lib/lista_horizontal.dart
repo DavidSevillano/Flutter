7ee8cebc239db617dbb25749a98a050b6d27a18e
David Sevillano <dvd.sevillano@gmail.com>
2024-12-11 09:36:33 +0100

    Ejercicio terminado

--- Lista_horizontal/lista_horizontal/lib/lista_horizontal.dart
+++ Lista_horizontal/lista_horizontal/lib/lista_horizontal.dart
@@ -6,25 +6,134 @@ class ListaHorizontal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: double.infinity,
-      width: double.infinity,
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 60),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Best Iranian Actors and Actresses",
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                Text(
-                  "March 2020",
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                ),
-              ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        margin: const EdgeInsets.only(left: 20, right: 20, top: 60, bottom: 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 300,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Best Iranian Actors and Actresses",
+                    style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "March 2020",
+                    style: TextStyle(fontSize: 12, color: Colors.black26),
+                  ),
+                ],
+              ),
             ),
-          ),
-      )
+            const SizedBox(height: 60),
+            Container(
+              height: 500,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(65),
+                          child: Image.network(
+                            "https://media.themoviedb.org/t/p/w235_and_h235_face/xak9L0fkIOkjSwJpz3SWLl94tVP.jpg",
+                            width: 150,
+                            height: 400,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        const Text(
+                          "Dyessa",
+                          style: TextStyle(fontSize: 24, fontFamily: "Raleway"),
+                        ),
+                        const Text(
+                          "Garcia",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black38,
+                              fontFamily: "Raleway"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    width: 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(65),
+                          child: Image.network(
+                            "https://media.themoviedb.org/t/p/w235_and_h235_face/3Xm4zDO3gUVU4OWZnhb983GnEGx.jpg",
+                            width: 150,
+                            height: 400,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        const Text(
+                          "Julia",
+                          style: TextStyle(fontSize: 24, fontFamily: "Raleway"),
+                        ),
+                        const Text(
+                          "Victoria",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black38,
+                              fontFamily: "Raleway"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    width: 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(65),
+                          child: Image.network(
+                            "https://media.themoviedb.org/t/p/w235_and_h235_face/UQsBo3oeK5aw9tNHpLA2XKrgQt.jpg",
+                            width: 150,
+                            height: 400,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        const Text(
+                          "Naresh",
+                          style: TextStyle(fontSize: 24, fontFamily: "Raleway"),
+                        ),
+                        const Text(
+                          "Agastya",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black38,
+                              fontFamily: "Raleway"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
